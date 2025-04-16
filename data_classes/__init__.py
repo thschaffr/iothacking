5781861ece94de5211e6c08d8bb3bffbebf4f14f
Thomas Schaffer <147936017+thschaffr@users.noreply.github.com>
2025-04-16 15:09:26 +0200

    Update __init__.py

--- data_classes/__init__.py
+++ data_classes/__init__.py
@@ -1,1 +1,9 @@
+# --- START OF FILE data_classes/__init__.py ---
 
+from .broker_settings import BrokerSettings
+from .client_settings import ClientSettings
+
+# Optionally add __all__ to control imports with *
+__all__ = ['BrokerSettings', 'ClientSettings']
+
+# --- END OF FILE data_classes/__init__.py ---
