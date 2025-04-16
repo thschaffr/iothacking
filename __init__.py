7f34554527bc0d57ede27f19b486f7d90f48f6af
Thomas Schaffer <147936017+thschaffr@users.noreply.github.com>
2025-04-16 15:09:50 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# --- START OF FILE data_classes/__init__.py ---
-
-from .broker_settings import BrokerSettings
-from .client_settings import ClientSettings
-
-# Optionally add __all__ to control imports with *
-__all__ = ['BrokerSettings', 'ClientSettings']
-
-# --- END OF FILE data_classes/__init__.py ---
